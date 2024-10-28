7c87f473556b9c4b9233201ab9272aaa9859696e
Juan Ortiz <juan.ortiz@ias.com.co>
2024-10-28 09:51:58 -0500

    feature: add recursive method to query dynamo

--- infrastructure/driven-adapters/dynamo-adapter/src/main/java/com/ias/DynamoReactiveEventAdapter.java
+++ infrastructure/driven-adapters/dynamo-adapter/src/main/java/com/ias/DynamoReactiveEventAdapter.java
@@ -1,5 +1,6 @@
 package com.ias;
 
+import com.google.gson.Gson;
 import com.ias.model.EventEntity;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
@@ -9,6 +10,11 @@ import software.amazon.awssdk.enhanced.dynamodb.DynamoDbAsyncTable;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedAsyncClient;
 import software.amazon.awssdk.enhanced.dynamodb.Key;
 import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.enhanced.dynamodb.model.PagePublisher;
+import software.amazon.awssdk.enhanced.dynamodb.model.QueryEnhancedRequest;
+
+import java.util.List;
+import java.util.Objects;
 
 
 @Repository
@@ -17,9 +23,12 @@ public class DynamoReactiveEventAdapter {
     private final DynamoDbEnhancedAsyncClient enhancedAsyncClient;
     private final DynamoDbAsyncTable<EventEntity> eventEntityDynamoDbAsyncTable;
 
-    public DynamoReactiveEventAdapter(DynamoDbEnhancedAsyncClient enhancedAsyncClient) {
+    private final Gson mapper;
+
+    public DynamoReactiveEventAdapter(DynamoDbEnhancedAsyncClient enhancedAsyncClient, Gson mapper) {
         this.enhancedAsyncClient = enhancedAsyncClient;
         this.eventEntityDynamoDbAsyncTable = enhancedAsyncClient.table(EventEntity.TABLE_NAME, TableSchema.fromBean(EventEntity.class));
+        this.mapper = mapper;
     }
 
     public Mono<EventEntity> findById(String id) {
@@ -34,6 +43,27 @@ public class DynamoReactiveEventAdapter {
 
     public Flux<EventEntity> findAll() {
         return Flux.from(eventEntityDynamoDbAsyncTable.scan().items());
+    }
+
+    private Mono<List<EventEntity>> getAllRecords(QueryEnhancedRequest request, List<EventEntity> entityList) {
+        PagePublisher<EventEntity> pagePublisher = eventEntityDynamoDbAsyncTable.scan();
+        return Mono.from(pagePublisher)
+                .map(pagePublisher1 -> {
+                            entityList.addAll(
+                                    pagePublisher1
+                                            .items()
+                                            .stream()
+                                            .toList()
+                            );
+                            return pagePublisher1;
+                        }
+                )
+                .filter(page -> Objects.nonNull(page.lastEvaluatedKey())
+                )
+                .flatMap(pageResponse -> getAllRecords(
+                        request.toBuilder().exclusiveStartKey(pageResponse.lastEvaluatedKey()).build(),
+                        entityList
+                )).switchIfEmpty(Mono.just(entityList));
     }
 
     public Mono<EventEntity> save(EventEntity eventEntity) {
